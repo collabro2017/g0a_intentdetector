da183ae3cb4f76ec51adaec82e83b13f6b56d86d
Juan Verhook <juanverhook@Juans-MacBook-Air.local>
2018-09-17 01:02:16 +0200

    Added Read Emails dir with emails dataset

---
+++ ReadEmails/ReadEmails.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import pickle
+
+print('reading data...')
+
+data = pd.read_csv('split_emails.csv')
+
+split_emails = data.values.tolist() # 10.000 emails
+
+emails = []
+
+for i in range(0,len(split_emails)):
+	# print('\n--------------------------------------------------------------------------------------')
+	sections = split_emails[i][2].split('\r\n')
+	inMailBody = False
+	content = {}
+	for j in sections:
+		# print(j)
+		# if('noreply' in str(j) or 'do not need reply' in str(j) ):
+		# 	print('**************************************************************************** \n '
+		# 		+ '***************************************************************************')
+		# 	print(j)
+		if j.strip() == '' and not inMailBody:
+			# print('**** MAIL BODY ****')
+			inMailBody = True
+			content['Message'] = ''
+			continue
+		if not inMailBody:				
+			if (len(j.split(':',1))>1):
+				content[j.split(':',1)[0]] = j.split(':',1)[1]
+		else:
+			content['Message'] = content['Message'] + j
+	emails.append(content)
+
+
+print('Writing pickle file emailsList: where each element of the list is a dictionary of emails.')
+
+with open('pkl/emailsList.pkl','wb') as f:
+    pickle.dump( emails, f )
+
+print('file written as pkl/emailsList.pkl')
